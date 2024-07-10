76d44deb00420c4fe624e8b24b94aa560f72f353
GwynBleidD <gbd.lin@gmail.com>
2024-07-11 01:12:28 +0200

    fix: programapi panels.
    Panels fetched from programapi had too many speakers. Fixed it by
    replacing them with just "Panel" as a title. To be fixed later properly.

--- seed_from_programapi.py
+++ seed_from_programapi.py
@@ -32,6 +32,7 @@ days = {
 
 avatar_blacklist = {
     "kuba_NsMw534.png",
+    "d3da8160dd1848c1194e15df4fb1051c_bFs9Mv0.jpg",
     "Profile_small_qVJUTKd.jpg",
 }
 
@@ -42,15 +43,17 @@ def add_to_rooms(day: str, rooms: list[str], event_data):
         days[day].setdefault(room_slug, []).append(event_data)
 
 
-def add_speaker_session_to_rooms(day: str, rooms: list[str], event):
+def add_speaker_session_to_rooms(day: str, rooms: list[str], event, speakers: list | None = None):
+    if not speakers:
+        speakers = event["speakers"]
     event_data = {
         "start": event["start"],
         "type": "talk",
         "title": event["title"],
         "language": "en",
         "authors": [
-            {"name": speaker["name"], "picture_url": speaker["avatar"] or None}
-            for speaker in event["speakers"]
+            {"name": speaker["name"], "picture_url": speaker.get("avatar") or None}
+            for speaker in speakers
         ]
     }
     for author in event_data["authors"]:
@@ -104,21 +107,21 @@ def seed_days():
                         add_speaker_session_to_rooms(day, event["rooms"], event)
                 elif session_type == "Panel":
                     if len(event['speakers']) > 0:
-                        print(event)
-                        continue
+                        add_speaker_session_to_rooms(day, event["rooms"], event, speakers=[{"name": "Panel"}])
                     else:
-                        add_non_speaker_session_to_rooms(day, event["rooms"], event)
+                        add_speaker_session_to_rooms(day, event["rooms"], event, speakers=[{"name": "Panel"}])
                 else:
                     print(event)
 
 
-def dump_to_toml(room, day, events, start_date: bool = True):
+def dump_to_toml(room, day, events, start_date: bool = True, **extra):
     path = Path("config/events/europython/2024") / room / f"{day}.toml"
     data = {
         "meeting": {
             "name": f"EuroPython 2024 {room} day {day}",
             "number": 2024,
             "schedule": events,
+            **extra,
         }
     }
     if start_date:
@@ -147,6 +150,7 @@ def dump_all_to_config():
         "test",
         all_events,
         start_date=False,
+        template={"ticker_source": "manual"}
     )
 
 
