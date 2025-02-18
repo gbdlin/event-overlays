b69dcf03617206e5d6d62330b6766e8860c0e64e
GwynBleidD <gbd.lin@gmail.com>
2025-02-18 09:14:48 +0100

    chore(deps): Dependency bump
    Bump dependencies and switch to the newest pydantic.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi_utilities import repeat_at
+from fastapi_utilities import repeat_every
 
 from .routes import old_router, update_schedule_ticker, v1_router
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await repeat_every(seconds=60)(update_schedule_ticker)()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/v1", v1_router, name="v1")
 app.mount("", old_router, name="old")
 
-app.on_event("startup")(repeat_at(cron="* * * * *")(update_schedule_ticker))
+
