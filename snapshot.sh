6b8bdda4634c4a8e523ab7829bbcc2e4a3a9b9d4
GwynBleidD <gbd.lin@gmail.com>
2025-03-21 23:55:02 +0100

    chore: Remove snapshotting from source files
    Snapshotting is a deployment-specific feature, shouldn't be included in
    the open-source repository.

--- snapshot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "AAAAAAAAAA"
-/home/gwynbleidd/.local/share/poetry/bin/poetry -C /home/gwynbleidd/Projects/pykonik/obs-overlays run python /home/gwynbleidd/Projects/pykonik/obs-overlays/snapshot.py "$@"
