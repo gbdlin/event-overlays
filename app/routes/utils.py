bf3bc6a2a916d92d05623a2c8ab6a8e17ff15a4a
GwynBleidD <gbd.lin@gmail.com>
2025-07-16 11:37:25 +0200

    feat: Schedule table variants
    - Added display settings to schedule table (internal) to hide some
      elements.
    - Fixed empty `tr` tag added to the schedule table

--- app/routes/utils.py
+++ app/routes/utils.py
@@ -2,11 +2,12 @@ from fastapi import Request
 from ..template_renderer import renderer
 
 
-async def schedule_table_view(request: Request, rig: str):
+async def schedule_table_view(request: Request, rig: str, display: str = "full"):
     return renderer.TemplateResponse(
         "schedule-table.html",
         {
             "request": request,
             "rig": rig,
+            "display_type": display,
         }
     )
